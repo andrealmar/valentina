b2a6aec564ca8c6eac616c430cead6ed43883ff2
Andre Almar <andre@y7mail.com>
2016-03-07 14:54:12 -0300

    secret_key to test travis CI build

--- valentina/settings.py
+++ valentina/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY', default='c-+q38ms6tuc^0)5=*3z(3w+d$v%kx^=q*di_&tq=#7b=!#q)2')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
